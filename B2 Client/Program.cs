1f2af5ed80c20edb069949d5e07afda8ff1160f4
trandainhan96 <dainhantran160396@gmail.com>
2017-10-13 20:59:10 +0700

    init project

---
+++ B2 Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace B2_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); 
+           
+        
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0); 
+            EndPoint Remote = (EndPoint)sender;
+
+            byte[] data = new byte[4];
+            client.ReceiveFrom(data, ref Remote);
+            int serverChoosen = BitConverter.ToInt32(data, 0);
+
+            Random rand = new Random();
+            int clientChoossen = rand.Next(0, 2);
+
+            if (clientChoosen == serverChoosen)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Hoa");
+                client.SendTo(send, Remote);
+            }
+
+            if (clientChoosen < serverChoosen)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thang");
+                client.SendTo(send, Remote);
+            }
+            if (clientChoosen > serverChoosen)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thua");
+                client.SendTo(send, Remote);
+            }
+        }
+    }
+}
---
+++ Bai 2  - B2 - server/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Bai_2____B2___server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); 
+            server.Bind(ipep);
+            Console.WriteLine("Waiting for a client..."); 
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0); 
+            EndPoint Remote = (EndPoint)(sender);
+
+            byte[] data = new byte[4];
+            server.ReceiveFrom(data, ref Remote);
+            int clientChoosen = BitConverter.ToInt32(data, 0);
+
+            Random rand = new Random();
+            int serverChoosen = rand.Next(0, 2);
+
+            if (clientChoosen == serverChoosen)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Hoa");
+                server.SendTo(send, Remote);
+            }
+
+            if (clientChoosen < serverChoosen)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thang");
+                server.SendTo(send, Remote);
+            }
+            if (clientChoosen > serverChoosen)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thua");
+                server.SendTo(send, Remote);
+            }
+        }
+    }
+}
